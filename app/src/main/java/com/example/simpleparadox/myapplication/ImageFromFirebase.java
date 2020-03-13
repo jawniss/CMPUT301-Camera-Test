0ca00b76f59bcbc4dd0e8f3b6f16d8a48dc6bfd4
jawniss <johnas@ualberta.ca>
2020-03-13 14:50:44 -0600

    Second activity not getting right image from Firebase cus the URL is invalid, leads to no image

--- app/src/main/java/com/example/simpleparadox/myapplication/ImageFromFirebase.java
+++ app/src/main/java/com/example/simpleparadox/myapplication/ImageFromFirebase.java
@@ -14,13 +14,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.io.InputStream;
 import java.net.URL;
 
 public class ImageFromFirebase extends AppCompatActivity {
 
-    ImageView mImageView;
+    ImageView firebaseImage;
     DatabaseReference reff;
 
     @Override
@@ -28,19 +29,23 @@ public class ImageFromFirebase extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_from_firebase);
 
-        mImageView = findViewById( R.id.firebase_image );
+        firebaseImage = findViewById( R.id.firebase_image );
 
         reff = FirebaseDatabase.getInstance().getReference().child("Image Uploads").child("Hi");
 
         reff.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String url = dataSnapshot.child("imageUrl").getValue().toString();
-                Drawable imageToDraw = LoadImageFromWebOperations( url );
+//                String url = dataSnapshot.child("imageUrl").getValue().toString();
+//                Drawable imageToDraw = LoadImageFromWebOperations( url );
+                String url = "https://firebasestorage.googleapis.com/v0/b/project-demo-70f7b.appspot.com/o/Image%20Uploads%2F1584129100555.jpg?alt=media&token=92736b9f-1633-45f7-b160-7da35d66937f";
                 Log.d("Firebaseee", url);
 
-                mImageView.setImageDrawable( imageToDraw );
+//                mImageView.setImageDrawable( imageToDraw );
 
+                Picasso.get()
+                        .load( url )
+                        .into( firebaseImage );
             }
 
             @Override
